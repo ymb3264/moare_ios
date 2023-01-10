27c16462199e0349ab3b1f0b8919a1ee9251a604
ymb3264 <a01028371042@gmail.com>
2023-01-10 19:46:44 +0900

    Initial Commit

---
+++ moare/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  moare
+//
+//  Created by Mobul Yoon on 2023/01/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ moare/moareApp.swift
@@ -0,0 +1,17 @@
+//
+//  moareApp.swift
+//  moare
+//
+//  Created by Mobul Yoon on 2023/01/10.
+//
+
+import SwiftUI
+
+@main
+struct moareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
